ff6ef7cf3d5b19bb911c0a7ba0cbf811006f1d95
Muhammad Shahzaib <shaibimaibi501@gmail.com>
2020-06-06 03:25:09 +0500

    all the changes

--- website/shop/urls.py
+++ website/shop/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path("",views.index,name="sindex"),
-    path("about",views.index,name="aboutUs"),
-    path("contact",views.index,name="contactUs"),
-    path("tracker",views.index,name="tracker"),
-    path("search",views.index,name="search"),
-    path("productview",views.index,name="prodview"),
-    path("checkout",views.index,name="checkout"),
+    path("",views.home,name="sindex"),
+    
+    path("contact",views.contact,name="contactUs"),
+    
+
 
 
 ]
--- website/shop/views.py
+++ website/shop/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import product
+from math import ceil
+def home(request):
+    myProducts = product.objects.all()
+    n = len(myProducts)
+    nslides = n//8 + ceil((n/8)-(n//8))
+    mydict = {'products':myProducts,'no-of-slides':nslides, 'range':range(1,nslides)}
+    return render(request,'shop/Sindex.html',mydict)
 
-def index(request):
-    return render(request,'shop/Sindex.html')
-
+def contact(request):
+    return render(request,'shop/contact-section.html')
